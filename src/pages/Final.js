ae2989e3479767e93d4d51374fbfac924b0c18a3
janel lucia <thejanellucia@gmail.com>
2020-02-09 18:32:40 -0500

    minor adjustments

--- src/pages/Final.js
+++ src/pages/Final.js
@@ -15,7 +15,7 @@ function ConceptOne() {
   return (
     <div className="concept-one">
       <h3>Final Designs</h3>
-      <p>After finding out your preferences I brought all your thoughts together in the final concept. You asked to see the colours from Concept 2 (I did adjust 'Pearl White', 'Light Apricot' and 'Golden Stone' slightly to compliment the layout in a more flattering way) along with the Logo and tiles from Concept 3 (as well as 2 tiles from concept 1) </p>
+      <p>After finding out your preferences I brought all your thoughts together in the final concept. You asked to see the colours from Concept 2 (I added 'Sage' and adjusted 'Pearl White', 'Light Apricot' and 'Golden Stone' slightly to compliment this layout in a more flattering manner). You wanted the Logo and Tiles from Concept 3 (and 2 tiles from concept 1) </p>
       <p>You asked to see Glacial Indifference replace Commuters Sans and after playing around with that idea, I disagree. The short and wide characters present in Commuters Sans balance the high and thin character sets present in Orpheus and Capitolium. Those characteristics just aren't present in Glacial Indifference.</p>
       <p>That being said, we can go with the fonts from concept 1 or 2 instead of 3 but I recommend we stick with this font combination, Commuters Sans adds depth and gives this combination and classy, luxurious vibe. I also added the fonts to this site so you can preview what everything can look like once it's all together. But, if you are truly unhappy with Commuter Sans, let's talk it out.</p>
       <img src={vision} alt="vision board" />
