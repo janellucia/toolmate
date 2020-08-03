f98130fb435d7b39fef0c7ee075fce418082a893
janel lucia <thejanellucia@gmail.com>
2020-08-03 17:43:21 -0400

    final concepts

--- src/pages/ConceptRevised.js
+++ src/pages/ConceptRevised.js
@@ -18,9 +18,7 @@ import letterFinal from '../images/concepts/final/letterhead.png';
 function ConceptTwo() {
   return (
     <div className="concept-two">
-      <h3>Second Draft</h3>
-      <p>After hearing your feedback and getting more inspo from you I came up with this concept. You mentioned you like the gradients and you have always used iridescent in your inspiration, so I played mostly into that. I wanted to play with different angles of what a shiny iridescent texture can look like. You liked the layout so no major changes to that.</p>
-      <p>You were concerned that your logo would look like text and not like an actual logo and I put in the comparison for you to see that there were significant changes to the lettering, spacing and weights in order to make it unique to you. I also wanted to make sure your logo is easily recognizable to users, when they see your logo, they instantly know it's you. You also wanted some more detail in your icon, so after reviewing your values and listening to the inspo you’ve sent me over the last few days I wanted to incorporate a crystal, a crystal that represents growth, personal style and overall healing which is the message we are trying to send your audience. </p>
+      <h3>Final Draft</h3>
       <img src={visionFinal} alt="vision board" />
       <img src={colourFinal} alt="colour" />
       <img src={typeOneFinal} alt="brand typography" />
