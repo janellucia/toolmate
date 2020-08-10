8297c74d47be9140a6b748218210d8e7e139e8aa
janel lucia <thejanellucia@gmail.com>
2020-08-10 19:45:54 -0400

    branding final concepts

--- src/pages/Misc.js
+++ src/pages/Misc.js
@@ -7,7 +7,7 @@ import letterFinal from '../images/concepts/final/letterhead.png';
 function Misc() {
   return (
     <div className="concept-two">
-      <img src={cardsFinal} alt="business cards example" />
+      <img src={cardsFinal} alt="business cards example" className="biz-cards" />
       <img src={letterFinal} alt="site/typography preview" className="letterhead" />
     </div >
   );
