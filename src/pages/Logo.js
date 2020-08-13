38a18158acf994de80033e8a49dace0c2105d06e
janel lucia <thejanellucia@gmail.com>
2020-08-13 17:03:41 -0400

    update icons

--- src/pages/Logo.js
+++ src/pages/Logo.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import logo from '../images/concepts/final/logo.png';
 import logoAccent from '../images/concepts/final/logo-accent.png';
+import icons from '../images/concepts/final/icons.png';
 
 
 function Logos() {
   return (
     <div className="concept-one">
       <img src={logo} alt="logo exploration" />
-      <img src={logoAccent} alt="qube properties icon" />
+      <img src={logoAccent} alt="logo accent" />
+      <img src={icons} alt="icon exploration" />
     </div>
   );
 }
