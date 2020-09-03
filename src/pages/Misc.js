e2568446f48ea11ad7d64e8cd6d9eb5cc9db9895
janel lucia <thejanellucia@gmail.com>
2020-09-03 11:42:57 -0400

    print assets upload

--- src/pages/Misc.js
+++ src/pages/Misc.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 
-import cards from '../images/concepts/final/biz-cards.png';
-import letterhead from '../images/concepts/final/letterhead.png';
+import cards from '../images/concepts/print/biz-cards.png';
+import letterhead from '../images/concepts/print/letterhead.png';
+import signOne from '../images/concepts/print/landscape.png';
+import signTwo from '../images/concepts/print/portrait.png';
 
 
 function Moodboard() {
@@ -10,6 +12,8 @@ function Moodboard() {
     <div className="concept-one">
       <img src={cards} alt="business cards example" />
       <img src={letterhead} alt="letterhead preview" class="letterhead" />
+      <img src={signOne} alt="landscape sign" />
+      <img src={signTwo} alt="portrait sign" class="letterhead" />
     </div>
   );
 }
